83530e9e04d850d8c24245810d8de8c26dc14aaa
Tendai Karuwo <tendaikaruwo96@gmail.com>
2022-11-24 10:39:35 +0000

    new changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import QuizApp from "./Components/Quiz/QuizApp";
+import App from "./App";
 /* import CalendarApp from "./Components/Calendar/index"; */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <QuizApp />
+    <App />
     {/* <CalendarApp /> */}
   </React.StrictMode>
 );
